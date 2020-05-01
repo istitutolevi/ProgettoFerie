b5221deb25e4ae515d4732e0a7d667940fd6c5e9
andreamauro01 <andreamauro01@gmail.com>
2020-05-01 16:53:57 +0200

    Inizio Creazione Utente

---
+++ ProgettoFerie/app/CreaUser.js
@@ -0,0 +1,35 @@
+﻿($(document).ready(function () {
+
+
+    $("#but").click(function () {
+
+        var email = $("#txt_email").val();
+        var pwd = $("#txt_pw").val();
+        var Cpwd = $("#txt_cpw").val();
+        
+        
+
+
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:52642/api/Account/Register",
+            data: { txt_email:email, txt_pw:pwd, txt_cpw:Cpwd },
+            dataType: "json",
+            success: function (data) {
+                window.alert("registrazione avvenuta con successo");
+            } else {
+                window.alert("registrazione fallita");
+            }
+        });
+
+
+
+
+
+
+    });
+
+
+}));
+
