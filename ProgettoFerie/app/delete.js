eb2653077b96c127f0d7f47d80242f296f750069
andreamauro01 <andreamauro01@gmail.com>
2020-05-31 17:49:41 +0200

    -Apportate varie modifiche; -È stato aggiunto delete.html e delete.js (funzionante); -Aggiunto get_Ore_Data_Ora ( sia HTML e JS):funzionante ma da sistemare.

---
+++ ProgettoFerie/app/delete.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    $("#btn").click(function () {
+        var Id = document.getElementById('id').value;
+
+
+
+        $.ajax({
+            type: 'delete',
+            url: '/api/Ore/'+Id,
+            contentType: 'application/json',
+            // dataType: 'json',
+            success: function (data) {
+                window.alert("ok");
+
+            },
+            error: function () {
+                window.alert("errore");
+            }
+        });
+    });
+});
---
+++ ProgettoFerie/app/getOre_data_ora.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $("#btn").click(function () {
+        var Data = document.getElementById('date').value;
+        var Ora = document.getElementById('Nora').value;
+
+
+
+        $.ajax({
+            type: 'GET',
+            url: '/api/Ore?data='+Data+'&ora='+Ora,
+            dataType: 'json',
+            success: function (data) {
+                $.each(data, function (index, element) {
+
+                    $("#risultato").append(moment(element.Data).format('LLL') + " " + element.NumeroOra + " " + element.InsegnanteDisposizione.UserName + "<br>");
+                });
+            }
+        });
+    });
+});
