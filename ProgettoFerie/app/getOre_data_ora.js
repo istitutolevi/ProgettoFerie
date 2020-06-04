16200efe77e25ba6ee91267bd4101a6fd28582cc
andreamauro01 <andreamauro01@gmail.com>
2020-06-04 10:58:45 +0200

    Modificato getOre_data_ora.js (funzionante). Aggiunto hmtl e css per il login (da fare OAUTH2)

--- ProgettoFerie/app/getOre_data_ora.js
+++ ProgettoFerie/app/getOre_data_ora.js
@@ -12,8 +12,8 @@
             dataType: 'json',
             success: function (data) {
                 $.each(data, function (index, element) {
-
-                    $("#risultato").append(moment(element.Data).format('LLL') + " " + element.NumeroOra + " " + element.InsegnanteDisposizione.UserName + "<br>");
+                    $("#risultato").hide();
+                    $("#b").append(moment(element.Data).format('LLL') + " " + element.NumeroOra + " " + element.InsegnanteDisposizione.UserName + "<br>");
                 });
             }
         });
